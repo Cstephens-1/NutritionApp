b0feda70b811cd9174969b30bae09a5b65501315
Cory <corystephens012@gmail.com>
2023-01-27 16:50:01 -0500

    trying to add npm packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 // client/src/components/App.js
 import { useState, useEffect } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,14 +14,14 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Switch>
+        <Routes>
           <Route path="/testing">
             <h1>Test Route</h1>
           </Route>
           <Route path="/">
             <h1>Page Count: {count}</h1>
           </Route>
-        </Switch>
+        </Routes>
       </div>
     </BrowserRouter>
   );
