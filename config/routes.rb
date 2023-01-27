69411c09b720fa0e42a724a9063a5487ac662c99
Cory <corystephens012@gmail.com>
2023-01-27 16:28:04 -0500

    configured for client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index
+  def hello_world
+    session[:count] = (session[:count] || 0) + 1
+    render json: { count: session[:count] }
+  end
+
    get '*path',
       to: 'fallback#index',
       constraints: ->(req) { !req.xhr? && req.format.html? }
